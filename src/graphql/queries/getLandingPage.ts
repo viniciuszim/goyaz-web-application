e8d4ac6a4f4b41b68c8012006066fcd8bed94024
Vinicius Miguel <viniciuszim@hotmail.com>
2021-04-14 22:20:17 -0300

    alterando queries

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,5 +1,18 @@
 const GET_LANDING_PAGE = /* GraphQL */ `
   # Components
+  fragment AvaliacaoComp on ComponentComponentAvaliacao {
+    titulo
+    valor
+  }
+
+  fragment BotaoComp on ComponentComponentBotao {
+    label
+    link
+    linkExterno
+    primario
+    secundario
+  }
+
   fragment ImageComp on ComponentComponentImage {
     image {
       caption
@@ -7,6 +20,72 @@ const GET_LANDING_PAGE = /* GraphQL */ `
     }
     link
     linkExterno
+  }
+
+  fragment DepoimentoComp on ComponentComponentDepoimento {
+    imagem {
+      ...ImageComp
+    }
+    nome
+    cargo
+    descricao
+    ativo
+  }
+
+  fragment FaqComp on ComponentComponentFaq {
+    pergunta
+    resposta
+    ativo
+  }
+
+  fragment FormItemComp on ComponentComponentFormularioItem {
+    icone
+    nome
+    placeholder
+    obrigatorio
+  }
+
+  fragment FormComp on ComponentComponentFormulario {
+    titulo
+    descricao
+    itens {
+      ...FormItemComp
+    }
+    botao {
+      ...BotaoComp
+    }
+    ativo
+  }
+
+  fragment IconeDescComp on ComponentComponentIconeDescricao {
+    icone
+    titulo
+    descricao
+  }
+
+  fragment ListaTextoComp on ComponentComponentTextList {
+    label
+  }
+
+  fragment PrecoComp on ComponentComponentPreco {
+    destaque
+    tipo
+    moeda
+    valor
+    frequencia
+    botao {
+      ...BotaoComp
+    }
+    itens {
+      ...ListaTextoComp
+    }
+    ativo
+  }
+
+  fragment RedesSociaisComp on ComponentComponentRedesSociais {
+    icone
+    nome
+    link
   }
 
   # Pages
@@ -18,10 +97,191 @@ const GET_LANDING_PAGE = /* GraphQL */ `
     }
   }
 
+  fragment MenuPrincipalPage on LandingPage {
+    MenuPrincipal {
+      label
+      link
+      alternativo
+    }
+  }
+
+  fragment ParceirosPage on LandingPage {
+    Parceiros {
+      titulo
+      imagens {
+        ...ImageComp
+      }
+      ativo
+    }
+  }
+
+  fragment OQueFazemosPage on LandingPage {
+    OQueFazemos {
+      titulo
+      descricao
+      lista {
+        ...IconeDescComp
+      }
+      ativo
+    }
+  }
+
+  fragment ComoPodemosAjudarPage on LandingPage {
+    ComoPodemosAjudar {
+      imagem {
+        ...ImageComp
+      }
+      titulo
+      descricao
+      TextList {
+        ...ListaTextoComp
+      }
+      botao {
+        ...BotaoComp
+      }
+      ativo
+    }
+  }
+
+  fragment BannerPrincipalPage on LandingPage {
+    BannerPrincipal {
+      imagens {
+        ...ImageComp
+      }
+      titulo
+      descricao
+      link
+      formulario {
+        ...FormComp
+      }
+      ativo
+    }
+  }
+
+  fragment Banner2Page on LandingPage {
+    Banner2 {
+      imagem {
+        ...ImageComp
+      }
+      icones {
+        ...IconeDescComp
+      }
+      ativo
+    }
+  }
+
+  fragment NumerosPage on LandingPage {
+    Numeros {
+      background {
+        ...ImageComp
+      }
+      titulo
+      itens {
+        ...IconeDescComp
+      }
+      ativo
+    }
+  }
+
+  fragment DepoimentosPage on LandingPage {
+    Depoimentos {
+      titulo
+      descricao
+      avaliacoes {
+        ...AvaliacaoComp
+      }
+      depoimentos {
+        ...DepoimentoComp
+      }
+      ativo
+    }
+  }
+
+  fragment Banner3Page on LandingPage {
+    Banner3 {
+      background {
+        ...ImageComp
+      }
+      titulo
+      descricao
+      botoes {
+        ...BotaoComp
+      }
+      ativo
+    }
+  }
+
+  fragment CaracteristicasPage on LandingPage {
+    Caracteristicas {
+      titulo
+      descricao
+      itens {
+        ...IconeDescComp
+      }
+      ativo
+    }
+  }
+
+  fragment PrecoPage on LandingPage {
+    Preco {
+      titulo
+      descricao
+      itens {
+        ...PrecoComp
+      }
+      ativo
+    }
+  }
+
+  fragment PerguntasFrequentesPage on LandingPage {
+    PerguntasFrequentes {
+      titulo
+      descricao
+      itens {
+        ...FaqComp
+      }
+      ativo
+    }
+  }
+
+  fragment FaleConoscoPage on LandingPage {
+    FaleConosco {
+      titulo
+      descricao
+      button {
+        ...BotaoComp
+      }
+      ativo
+    }
+  }
+
+  fragment SocialMidiaPage on LandingPage {
+    SocialMidia {
+      itens {
+        ...RedesSociaisComp
+      }
+      ativo
+    }
+  }
+
   # Main
   query GET_LANDING_PAGE {
     landingPage {
       ...LogoPage
+      ...MenuPrincipalPage
+      ...BannerPrincipalPage
+      ...ParceirosPage
+      ...OQueFazemosPage
+      ...ComoPodemosAjudarPage
+      ...Banner2Page
+      ...NumerosPage
+      ...DepoimentosPage
+      ...Banner3Page
+      ...CaracteristicasPage
+      ...PrecoPage
+      ...PerguntasFrequentesPage
+      ...FaleConoscoPage
+      ...SocialMidiaPage
     }
   }
 `
--- src/types/api.ts
+++ src/types/api.ts
@@ -1,4 +1,18 @@
 // # Components
+
+export type AvaliacaoComp = {
+  titulo: string
+  valor: string
+}
+
+export type BotaoComp = {
+  label: string
+  link: string
+  linkExterno: string
+  primario: boolean
+  secundario: boolean
+}
+
 export type ImageType = {
   caption: string
   url: string
@@ -10,13 +24,182 @@ export type ImageComp = {
   linkExterno: string
 }
 
+export type DepoimentoComp = {
+  imagem: ImageComp
+  nome: string
+  cargo: string
+  descricao: string
+  ativo: boolean
+}
+
+export type FaqComp = {
+  pergunta: string
+  resposta: string
+  ativo: boolean
+}
+
+export type FormItemComp = {
+  icone: string
+  nome: string
+  placeholder: string
+  obrigatorio: boolean
+}
+
+export type FormComp = {
+  titulo: string
+  descricao: string
+  itens: FormItemComp
+  botao: BotaoComp
+  ativo: boolean
+}
+
+export type IconeDescComp = {
+  icone: string
+  titulo: string
+  descricao: string
+}
+
+export type ListaTextoComp = {
+  label: string
+}
+
+export type PrecoComp = {
+  destaque: string
+  tipo: string
+  moeda: string
+  valor: number
+  frequencia: string
+  botao: BotaoComp
+  itens: ListaTextoComp
+  ativo: boolean
+}
+
+export type RedesSociaisComp = {
+  icone: string
+  nome: string
+  link: string
+}
+
 // # Pages
 export type LogoPageProps = {
   Logo: ImageComp
+}
+
+export type MenuPrincipalPageProps = {
+  label: string
+  link: string
+  alternativo: boolean
+}
+
+export type ParceirosPageProps = {
+  titulo: string
+  imagens: [ImageComp]
+  ativo: boolean
+}
+
+export type OQueFazemosPageProps = {
+  titulo: string
+  descricao: string
+  lista: [IconeDescComp]
+  ativo: boolean
+}
+
+export type ComoPodemosAjudarPageProps = {
+  imagem: ImageComp
+  titulo: string
+  descricao: string
+  TextList: [ListaTextoComp]
+  botao: BotaoComp
+  ativo: boolean
+}
+
+export type BannerPrincipalPageProps = {
+  imagens: ImageComp
+  titulo: string
+  descricao: string
+  link: string
+  formulario: FormComp
+  ativo: boolean
+}
+
+export type Banner2PageProps = {
+  imagem: ImageComp
+  icones: [IconeDescComp]
+  ativo: boolean
+}
+
+export type NumerosPageProps = {
+  background: ImageComp
+  titulo: string
+  itens: [IconeDescComp]
+  ativo: boolean
+}
+
+export type DepoimentosPageProps = {
+  titulo: string
+  descricao: string
+  avaliacoes: [AvaliacaoComp]
+  depoimentos: [DepoimentoComp]
+  ativo: boolean
+}
+
+export type Banner3PageProps = {
+  background: ImageComp
+  titulo: string
+  descricao: string
+  botoes: [BotaoComp]
+  ativo: boolean
+}
+
+export type CaracteristicasPageProps = {
+  titulo: string
+  descricao: string
+  itens: [IconeDescComp]
+  ativo: boolean
+}
+
+export type PrecoPageProps = {
+  titulo: string
+  descricao: string
+  itens: [PrecoComp]
+  ativo: boolean
+}
+
+export type PerguntasFrequentesPageProps = {
+  titulo: string
+  descricao: string
+  itens: [FaqComp]
+  ativo: boolean
+}
+
+export type FaleConoscoPageProps = {
+  titulo: string
+  descricao: string
+  button: BotaoComp
+  ativo: boolean
+}
+
+export type SocialMidiaPageProps = {
+  itens: RedesSociaisComp
+  ativo: boolean
 }
 
 // # Main
 
 export type LandingPageProps = {
   Logo: LogoPageProps
+  MenuPrincipal: MenuPrincipalPageProps
+  BannerPrincipal: BannerPrincipalPageProps
+  Parceiros: ParceirosPageProps
+  OQueFazemos: OQueFazemosPageProps
+  ComoPodemosAjudar: ComoPodemosAjudarPageProps
+  Banner2: Banner2PageProps
+  Numeros: NumerosPageProps
+  Depoimentos: DepoimentosPageProps
+  Banner3: Banner3PageProps
+  Caracteristicas: CaracteristicasPageProps
+  Preco: PrecoPageProps
+  PerguntasFrequentes: PerguntasFrequentesPageProps
+  FaleConosco: FaleConoscoPageProps
+  SocialMidia: SocialMidiaPageProps
 }
